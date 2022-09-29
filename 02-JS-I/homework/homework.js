5d09aac11d562f2361e04f431b0938906269c8c7
agustin <agustin@quinteros.com>
2022-09-28 23:11:31 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "holva";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "queeee";
+const nuevoNum = "queee"
 
 // Crea una variable booleana:
 const nuevoBool = null;
